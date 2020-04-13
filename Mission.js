8ea4ca29afe5dd1ec3a0206caa63ae7c098dea8d
anyapetrovskaya <60788665+anyapetrovskaya@users.noreply.github.com>
2020-04-12 21:44:07 -0700

    factored out mission class

---
+++ Mission.js
@@ -0,0 +1,63 @@
+"use strict";
+
+function Mission(params) {
+	var o = new Object();
+	o.init = function(params) {
+		if (!params) return;
+		o.params = params;
+	};
+	o.scrollFields = ['mAction', 'mCategory', 'mTitle', 'mDesc', 'mPhoto', 'mDuration', 'mExp'];
+	o.params = {};
+	o.updateScroll = function() {
+		for (let eid of o.scrollFields) o.updateElement(eid);		
+	};
+	o.handleAction = function() { o.log("not implemented"); };	
+	o.updateElement = function(eid) {
+		if (!o.params[eid]) return;
+		let s = o.params[eid];
+		if (o.fieldFormat[eid]) s = eval(o.fieldFormat[eid]);
+		o.setElement(eid, s);
+	};	
+	o.fieldFormat = {
+			mPhoto: "`<img style='vertical-align:top;height:125px;' src='${o.params[eid]}'>`"
+	};
+	o.setElement = function(eid, s) {
+		if (!document.getElementById(eid)) return;
+		document.getElementById(eid).innerHTML = s;
+	};
+	o.log = function(s) { o.setElement('errorLog', s); };
+	o.init(params);
+	return o;
+}
+
+
+function AgentMission(params) {
+	var o = new Mission(params);
+	o.handleAction = function() {
+		o.log("calling geo...");
+		navigator.geolocation.getCurrentPosition(o.handleGeo, o.handleGeoError, o.geoOpt);
+	};
+	o.handleGeo = function(pos) {
+		console.log("position");
+		let crd = pos.coords;
+		o.log(`You are at ${crd.latitude}, ${crd.longitude} up to ${crd.accuracy} meters.`);
+		if (!o.params.mCoord) {
+			o.log('No geo coords defined in this mission.');
+			return;
+		}
+		let mCoord = o.params.mCoord; //{ lat: 37.345749, lon: -122.070328 };
+		let d = getDistanceFromLatLonInM(mCoord.lat,mCoord.lon,crd.latitude,crd.longitude);
+		o.log(`distance ${d.toFixed(2)} meters with ${crd.accuracy} meters accuracy.`);
+	};
+	o.handleGeoError = function(err) {
+		let s = `geo error(${err.code}): ${err.message}`;
+		console.log(s);
+		o.log(s);
+	};
+	o.geoOpt = {
+		  enableHighAccuracy: true,
+		  timeout: 5000,
+		  maximumAge: 0
+	};
+	return o;
+}
--- main.js
+++ main.js
@@ -8,57 +8,57 @@ function MissionHandler() {
 	var o = new Object();
 	o.selectedMission = undefined;
 	o.missions = {
-		mi1: { 	
-			mDesc: 'Agent: locate secret hideout.',
+		mi1: new AgentMission({ 	
+			mShort: 'Agent: locate secret hideout.',
 			mCategory: 'Agent Mission',
 			mTitle: 'Locate Secret Hideout',
-			mDetail: 'Ninja Alliance intelligence reports that there is a secret terrorist hideout near your location. Our agents discovered this photo of the hideout. Locate the hideout and report its whereabouts.',
+			mDesc: 'Ninja Alliance intelligence reports that there is a secret terrorist hideout near your location. Our agents discovered this photo of the hideout. Locate the hideout and report its whereabouts.',
 			mPhoto: 'resources/img/grant-park.png',
 			mCoord: { lat: 37.345749, lon: -122.070328 },
 			mDuration: '15 min',
 			mExp: '2,000 xp',
 			mAction: 'Report'
-			},
-		mi2: { 	
-			mDesc: 'Scholar: sharpen your mind.',
+			}),
+		mi2: new Mission({ 	
+			mShort: 'Scholar: sharpen your mind.',
 			mCategory: 'Scholar Mission',
 			mTitle: 'Sharpen Your Mind',
-			mDetail: 'Sharp mind is the hallmark of a master ninja.  Study math on Khan Academy to sharpen your mind.',
+			mDesc: 'Sharp mind is the hallmark of a master ninja.  Study math on Khan Academy to sharpen your mind.',
 			mPhoto: 'resources/img/ninja-study.png',					
 			mDuration: '45 min',
 			mExp: '6,000 xp',
 			mAction: 'Finish'
-			},
-		mi3: { 	
-			mDesc: 'Operative: dispose of contaminants.',
+			}),
+		mi3: new Mission({ 	
+			mShort: 'Operative: dispose of contaminants.',
 			mCategory: 'Operative Mission',
 			mTitle: 'Dispose of Contaminants',
-			mDetail: 'Clan hideout is filling up with contaminants.  Collect trash and dispose of it in the bins outside.',
+			mDesc: 'Clan hideout is filling up with contaminants.  Collect trash and dispose of it in the bins outside.',
 			mPhoto: 'resources/img/trash-cat.png',
 			mDuration: '10 min',
 			mExp: '1,500 xp',
 			mAction: 'Finish'
-			},
-		mi4: { 	
-			mDesc: 'Survivor: tame silver wolf.',
+			}),
+		mi4: new Mission({ 	
+			mShort: 'Survivor: tame silver wolf.',
 			mCategory: 'Survivor Mission',
 			mTitle: 'Tame Silver Wolf',
-			mDetail: "Learning to survive in the wilderness is important for any ninja.  For today's lesson, tame a silver wolf in Ark: Survival Evolved.  Submit screenshot as proof.",
+			mDesc: "Learning to survive in the wilderness is important for any ninja.  For today's lesson, tame a silver wolf in Ark: Survival Evolved.  Submit screenshot as proof.",
 			mPhoto: 'resources/img/silver-wolf.jpg',
 			mDuration: '15 min',
 			mExp: '1,000 xp',
 			mAction: 'Submit'
-		},
-		mi5: { 	
-			mDesc: 'Warrior: practice martial arts.',
+		}),
+		mi5: new Mission({ 	
+			mShort: 'Warrior: practice martial arts.',
 			mCategory: 'Warrior Mission',
 			mTitle: 'Practice Martial Arts',
-			mDetail: 'A ninja is a warrior.  And a warrior must hone combat skills.  Follow the <a target="_blank" href=https://www.youtube.com/watch?v=gv1_HgYIitk>sensei</a> to practice kick techniques.',
+			mDesc: 'A ninja is a warrior.  And a warrior must hone combat skills.  Follow the <a target="_blank" href=https://www.youtube.com/watch?v=gv1_HgYIitk>sensei</a> to practice kick techniques.',
 			mPhoto: 'resources/img/martial-panda.png',
 			mDuration: '10 min',
 			mExp: '1,000 xp',
 			mAction: 'Finish'
-		}
+		})
 	};
 	o.handleMissionClick = function(mid) {
 		if (o.missions[mid] == undefined) {
@@ -72,52 +72,14 @@ function MissionHandler() {
 			if (o.selectedMission) document.getElementById(o.selectedMission).style.backgroundColor = '#000000';
 			o.selectedMission = mid;
 			document.getElementById(mid).style.backgroundColor = '#8f8f8f';
-			o.updateMissionDetail(mid);
+			o.missions[mid].updateScroll();
 		}
 //		console.log(document.getElementById(mid).style.backgroundColor);
 	};
-	o.updateMissionDetail = function(mid) {
-		let mp = o.missions[mid];
-		if (!mp) return;
-		let eids = ['mAction', 'mCategory', 'mTitle', 'mDetail', 'mPhoto', 'mDuration', 'mExp'];
-		for (let eid of eids) o.updateElement(eid, mp);		
-	};
-	o.updateElement = function(eid, mp) {
-		if (!mp[eid]) return;
-		let pre = o.pre[eid] ? o.pre[eid] : '';
-		let post = o.post[eid] ? o.post[eid] : '';
-		document.getElementById(eid).innerHTML = pre + mp[eid] + post;
-	};
-	
-	o.pre = {
-			mPhoto: "<img style='vertical-align:top;height:125px;' src='"					
-	};
-	o.post = {
-			mPhoto: "'>"					
-	};
-	o.handleGeo = function(pos) {
-		console.log("position");
-//		console.log(pos);
-		let crd = pos.coords;
-		o.log(`You are at ${crd.latitude}, ${crd.longitude} up to ${crd.accuracy} meters.`);
-		let mCoord = { lat: 37.345749, lon: -122.070328 };
-		let d = getDistanceFromLatLonInM(mCoord.lat,mCoord.lon,crd.latitude,crd.longitude);
-		o.log(`distance ${d} meters with ${crd.accuracy} meters accuracy.`);
-	};
-	o.handleGeoError = function(err) {
-		let s = `geo error(${err.code}): ${err.message}`;
-		console.log(s);
-		o.log(s);
-	};
-	o.geoOpt = {
-		  enableHighAccuracy: true,
-		  timeout: 5000,
-		  maximumAge: 0
-	};
 	o.handleMissionAction = function() {
 		if (!o.selectedMission) return;
-		o.log("calling geo...");
-		navigator.geolocation.getCurrentPosition(o.handleGeo, o.handleGeoError, o.geoOpt);
+		if (!o.missions[o.selectedMission]) return;
+		o.missions[o.selectedMission].handleAction();
 	};
 	o.log = function(s) {
 		document.getElementById('errorLog').innerHTML = s;
@@ -137,7 +99,10 @@ function reportVersion() {
 	mh.log("version 6");
 };
 
-
+function init() {
+	reportVersion();
+	missionClick('mi1');
+};
 
 
 
